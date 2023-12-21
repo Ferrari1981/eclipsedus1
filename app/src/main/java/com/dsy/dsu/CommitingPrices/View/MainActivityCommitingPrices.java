715309b76e7d2dfcc308485e027f7477ee701a74
Ferrari1981 <xiaomimoraru.part17@gmail.com>
2023-12-21 15:22:40 +0300

    android studio 2023   декабрь

--- app/src/main/java/com/dsy/dsu/CommitingPrices/View/MainActivityCommitingPrices.java
+++ app/src/main/java/com/dsy/dsu/CommitingPrices/View/MainActivityCommitingPrices.java
@@ -1,16 +1,41 @@
 package com.dsy.dsu.CommitingPrices.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 
 import com.dsy.dsu.R;
 
-public class MainActivityCommitingPrices extends AppCompatActivity {
+import dagger.hilt.android.scopes.ActivityScoped;
 
+public class MainActivityCommitingPrices extends AppCompatActivity {
+    private FragmentManager fragmentManager;
+    private Activity activity;
+    private Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_commiting_prices);
+        // TODO: 21.12.2023
+    }
+
+
+}//todo  end  public class MainActivityCommitingPrices extends AppCompatActivity
+
+
+
+
+// TODO: 21.12.2023 class bicces logic
+class Bl{
+    private Activity activity;
+    private Context context;
+
+
+    public Bl(Activity activity, Context context) {
+        this.activity = activity;
+        this.context = context;
     }
 }
